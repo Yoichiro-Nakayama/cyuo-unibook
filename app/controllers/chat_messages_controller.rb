1f3ef560071c767713c8f896686d605a5317637f
仲山陽一朗 <naka10259429@nakasanyouichirou-no-MacBook-Air.local>
2016-03-12 19:33:22 +0900

    メール機能実装

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -6,8 +6,20 @@ class ChatMessagesController < ApplicationController
     @chat_message = ChatMessage.new
   end  
   def create
+    @chat = Chat.find(params[:chat_id])
+    @product = Product.find(params[:product_id])
+     if current_user.id == @chat.buyer_id
+     @user_id = @chat.seller_id
+     else 
+     @user_id = @chat.buyer_id
+     end
+
+    @user = User.find(@user_id)
+    @mail = UserMailer.complete_review(@user, @product, @chat)
     @chat_message = ChatMessage.create(create_params)
+    @mail.deliver # if Rails.env.production?
     redirect_to controller: :chat_messages, action: :new
+
   end
 
 
@@ -17,3 +29,17 @@ class ChatMessagesController < ApplicationController
     params.require(:chat_message).permit(:message).merge( product_id: params[:product_id], chat_id: params[:chat_id], user_id: current_user.id )
   end
 end
+
+
+
+    #  @chat = Chat.find(params[:chat_id])
+    #  if current_user = @chat.buyer_id
+    #  @user = @chat.seller_id
+    #  else
+    #  @user = @chat.buyer_id
+    #  end
+
+    # @mail = UserMailer.complete_review(@user)
+    #  @chat_message = ChatMessage.create(create_params)
+    # @mail.deliver # if Rails.env.production?
+    # redirect_to controller: :chat_messages, action: :new
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,13 @@
 class UserMailer < ApplicationMailer
+
+
+
+ def complete_review(user, product, chat)
+    @user = user
+    @chat = chat
+    @product = product
+    mail to: user.email, subject: "取引相手からメッセージがきました。UNIBOOK"
+  end
  
 end
+
